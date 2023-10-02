76070166670eba117c5f89f42765861a163cc19e
Jan <55839828+Piitschy@users.noreply.github.com>
2023-10-02 22:48:36 +0200

    finalize VVK

--- app/pages/admin/tickets/vvk.vue
+++ app/pages/admin/tickets/vvk.vue
@@ -1,18 +1,37 @@
-import { Scanner } from '../../../.nuxt/components';
 <script setup lang="ts">
 definePageMeta({
   layout: 'admin',
-
 });
+
+const pb = usePocketbase()
+
+let dialog = $ref(false)
+let id = $ref('')
 
 function onScan(url: string) {
   console.log(url)
-  const code = url.split('/').pop()
-  console.log(code)
+  const ticketId = url.split('/').pop()
+  if (!ticketId) return;
+  id = ticketId
+  dialog = true
+}
+
+async function sell() {
+  const record = await pb.collection('tickets').update<TicketRecord>(id, {sold: true});
+  if (!record.sold) {
+    alert('Verkauf fehlgeschlagen')
+  }
+  dialog = false
 }
 
 </script>
 
 <template>
   <Scanner class="full-screen" @on-scan="onScan($event)"/>
+  <v-dialog v-model="dialog">
+    <v-card class="pa-3 mx-auto" width="300px">
+      <v-card-title class="mx-auto">{{ id }}</v-card-title>
+      <v-btn class="mx-auto" width="128px" color="primary" @click="sell()">Verkaufen</v-btn>
+    </v-card>
+  </v-dialog>
 </template>
