9ec551988d14d0413166ad978bb1a1ee36961e7f
theCVDH <c.cooly1111@web.de>
2023-10-15 20:08:34 +0200

    Add cash calculator as Component

---
+++ app/pages/dev.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+  let val = $ref(0);
+  let _shown = $ref(false);
+</script>
+
+<template>
+  <cash-calculator :requested="val" :shown="_shown" @paied="_shown=false; val=0" @cancled="_shown=false"> </cash-calculator>
+  <input type="number" v-model="val">
+  <button @click="_shown=!_shown">+</button>
+</template>
