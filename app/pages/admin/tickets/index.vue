aa44a48065e76bdba89df16a5d5994b23c887857
Jan <55839828+Piitschy@users.noreply.github.com>
2023-10-16 21:34:30 +0200

    made a Scan tester

--- app/pages/admin/tickets/index.vue
+++ app/pages/admin/tickets/index.vue
@@ -3,48 +3,13 @@ definePageMeta({
   layout: 'admin',
 });
 
-const router = useRouter();
-
-let showScanner = $ref(false);
-
-function onScan(code: string) {
-  router.push({path: `/admin/tickets/${code}`});
-}
-
-function push(p: string) {
-  router.push({path: p});
+function onScan(s:string) {
+  console.log(s)
+  alert(s)
 }
 
 </script>
 
 <template>
-
-  <v-dialog width="90%" v-model="showScanner">
-    <v-card class="pa-3">
-      <scanner v-if="showScanner" @onScan="onScan($event)"></scanner>
-    </v-card>
-  </v-dialog>
-<div class="pa-3">
-  <!-- <v-card class="my-3" @click="push('/admin/tickets/create')">
-    <v-card-title>
-      Neues Ticket
-    </v-card-title>
-  </v-card> -->
-
-  <v-card class="my-3" @click="push('/admin/tickets/register')">
-    <v-card-title>
-      Papierticket verkaufen
-    </v-card-title>
-  </v-card>
-
-  <v-card class="my-3" @click="showScanner = true">
-    <v-card-title>
-      Ticket kontollieren
-    </v-card-title>
-  </v-card>
-  <!-- <div style="position: absolute; height: 100vh;" class="mr-8">
-    <Scanner @on-scan="onScan($event)"/>
-  </div> -->
-
-</div>
+<Scanner class="full-screen" @on-scan="onScan($event)"/>
 </template>
