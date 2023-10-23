08a3ec60d91499f1b00fe8591fa7c7d430f6e835
Jan <55839828+Piitschy@users.noreply.github.com>
2023-10-23 21:15:53 +0200

    Update nuxt.config.ts

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -37,6 +37,7 @@ export default defineNuxtConfig({
   },
   app: {
     head: {
+      title: 'Silent App',
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
       link: [
