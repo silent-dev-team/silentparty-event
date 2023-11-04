96ed5c58bf557b055a453db6ae8bb72135703684
Jan <55839828+Piitschy@users.noreply.github.com>
2023-11-04 13:12:21 +0100

    bigger font

--- app/components/kasse/CartItem.vue
+++ app/components/kasse/CartItem.vue
@@ -7,7 +7,12 @@ const { title,price,amount } = $defineProps<{
 </script>
 
 <template>
-    <div class="d-flex w-100 justify-space-between px-4 py-2">
-        <div class="self">{{amount}}x {{ title }}</div> <div style="align-self: end; font-weight: bolder;">{{parseFloat(""+price).toFixed(2)}} €</div>
+    <div class="d-flex w-100 justify-space-between px-4 py-2 text-h5">
+        <div class="self">
+          {{amount}}x {{ title }}
+        </div> 
+        <div style="align-self: end; font-weight: bolder;">
+          {{parseFloat(""+price).toFixed(2)}} €
+        </div>
     </div>
 </template>
