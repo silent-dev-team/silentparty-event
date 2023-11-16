3426b96689df32d925cf293f4f9add22ab044a5f
Jan <55839828+Piitschy@users.noreply.github.com>
2023-11-16 19:52:27 +0100

    Add SendMail function to hooks/tickets.go

--- pocketbase/hooks/tickets.go
+++ pocketbase/hooks/tickets.go
@@ -2,8 +2,10 @@ package hooks
 
 import (
 	"fmt"
+	"net/mail"
 
 	"github.com/pocketbase/pocketbase/core"
+	"github.com/pocketbase/pocketbase/tools/mailer"
 	"github.com/silent-dev-team/silentparty-event/pocketbase/utils"
 )
 
@@ -17,5 +19,33 @@ func GeneratePin() func(e *core.RecordCreateEvent) error {
 		pin := utils.GeneratePin()
 		e.Record.Set("_pin", pin)
 		return nil
+	}
+}
+
+func SendMail(app core.App) func(e *core.RecordUpdateEvent) error {
+	return func(e *core.RecordUpdateEvent) error {
+		sold := e.Record.GetBool("sold")
+		filled := e.Record.GetBool("filled")
+		used := e.Record.GetBool("used")
+		if used || !sold || (sold && !filled) {
+			return nil
+		}
+		message := &mailer.Message{
+			From: mail.Address{
+				Address: app.Settings().Meta.SenderAddress,
+				Name:    app.Settings().Meta.SenderName,
+			},
+			To:      []mail.Address{{Address: e.Record.GetString("email")}},
+			Subject: "Dein personalisiertes Ticket für die Silent Party",
+			HTML:    "<h1>Herzlichen Glückwunsch zur Personalisierung</h1><p>Hallo " + e.Record.GetString("firstName") + ",</p><p>du hast dein Ticket für die Silentparty am 18.11.23 erfolgreich personalisiert.</p><p>Wir freuen uns auf dich!</p><p>Dein Silent Party Team</p><p><img src=\"https://www.silentparty-hannover.de/images/logo.png\" alt=\"Silent Party Logo\" width=\"200\" height=\"200\"></p><p><a href=\"https://www.silentparty-hannover.de\">www.silentparty-hannover.de</a></p>",
+			// bcc, cc, attachments and custom headers are also supported...
+		}
+
+		// send message
+		err := app.NewMailClient().Send(message)
+		if err != nil {
+			fmt.Println(err)
+		}
+		return err
 	}
 }
--- pocketbase/main.go
+++ pocketbase/main.go
@@ -61,7 +61,6 @@ func main() {
 	app.OnBeforeServe().Add(routes.GetUserStats("/api/v1/userstats"))
 
 	app.OnRealtimeAfterSubscribeRequest().Add(sse.OnSubscription(app))
-
 	app.OnRealtimeBeforeMessageSend().Add(sse.OnMessage(app))
 
 	/* CUSTOM HOOKS */
@@ -125,32 +124,8 @@ func main() {
 		return nil
 	})
 
-	// send mail after ticket is sold
-	// app.OnRecordAfterUpdateRequest("tickets").Add(func(e *core.RecordUpdateEvent) error {
-	// 	sold := e.Record.GetBool("sold")
-	// 	filled := e.Record.GetBool("filled")
-	// 	used := e.Record.GetBool("used")
-	// 	if used || !sold || (sold && !filled) {
-	// 		return nil
-	// 	}
-	// 	message := &mailer.Message{
-	// 		From: mail.Address{
-	// 			Address: app.Settings().Meta.SenderAddress,
-	// 			Name:    app.Settings().Meta.SenderName,
-	// 		},
-	// 		To:      []mail.Address{{Address: e.Record.GetString("email")}},
-	// 		Subject: "Dein personalisiertes Ticket für die Silent Party",
-	// 		HTML:    "<h1>Herzlichen Glückwunsch zur Personalisierung</h1><p>Hallo " + e.Record.GetString("firstName") + ",</p><p>du hast dein Ticket für die Silentparty am 18.11.23 erfolgreich personalisiert.</p><p>Wir freuen uns auf dich!</p><p>Dein Silent Party Team</p><p><img src=\"https://www.silentparty-hannover.de/images/logo.png\" alt=\"Silent Party Logo\" width=\"200\" height=\"200\"></p><p><a href=\"https://www.silentparty-hannover.de\">www.silentparty-hannover.de</a></p>",
-	// 		// bcc, cc, attachments and custom headers are also supported...
-	// 	}
-
-	// 	// send message
-	// 	err := app.NewMailClient().Send(message)
-	// 	if err != nil {
-	// 		fmt.Println(err)
-	// 	}
-	// 	return err
-	// })
+	// send mail on ticket update
+	// app.OnRecordAfterUpdateRequest("tickets").Add(hooks.SendMail(app))
 
 	/* SERVE */
 
