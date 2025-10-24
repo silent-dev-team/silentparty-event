0eb4084558762eb9c51445cc41a09b309eca9015
Jan <jan-d.pietsch@live.de>
2025-10-24 20:06:14 +0200

    subtract teamreserve from lent hps

--- pocketbase/measurements/userstats.go
+++ pocketbase/measurements/userstats.go
@@ -122,7 +122,7 @@ func GetUserStats(app core.App) (*UserStats, error) {
 		ActiveAK:    activeAkTickets,
 		OpenAK:      clacOpenAKTickets(),
 		ReservedAK:  reservedAkTickets,
-		LentHPs:     len(lentHps) - teamReserve.Value,
+		LentHPs:     max(len(lentHps)-teamReserve.Value, 0),
 		UnusedHPs:   unusedHps,
 		Overbooks:   overbooks.Value,
 	}
